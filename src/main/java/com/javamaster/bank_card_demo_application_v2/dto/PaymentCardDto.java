4b1be3785f23827dbf4f805fd413e8dfcb4cda32
Ramil Vildanov <ramvildan.work@gmail.com>
2023-02-17 01:37:21 +0300

    Dto descriptions with validation rules

--- src/main/java/com/javamaster/bank_card_demo_application_v2/dto/PaymentCardDto.java
+++ src/main/java/com/javamaster/bank_card_demo_application_v2/dto/PaymentCardDto.java
@@ -2,7 +2,11 @@ package com.javamaster.bank_card_demo_application_v2.dto;
 
 import com.javamaster.bank_card_demo_application_v2.entity.type.CardType;
 import com.javamaster.bank_card_demo_application_v2.entity.type.CurrencyType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +16,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "Payment card information")
 public class PaymentCardDto {
 
+    @Schema(description = "Payment card Id")
+    @Min(1)
+    @Max(99999999)
     private Integer id;
 
+    @Schema(description = "Card number")
+    @Size(min = 1, max = 20)
     @NotBlank(message = "Card number must be specified")
     private Integer cardNumber;
 
